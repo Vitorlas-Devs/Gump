e2749827e99c3f93ab9d748e2d313e05ae1f914f
Menyhárt Attila <menyhart.atika@gmail.com>
2023-01-18 10:24:39 +0100

    hotfix

--- db_script/createDb.cmd
+++ db_script/createDb.cmd
@@ -1,5 +1,6 @@
 mongoimport --db=gump --collection=adverts --drop Data/adverts.json
 mongoimport --db=gump --collection=badges --drop Data/badges.json
+mongoimport --db=gump --collection=categories --drop Data/categories.json
 mongoimport --db=gump --collection=partners --drop Data/partners.json
 mongoimport --db=gump --collection=recipes --drop Data/recipes.json
 mongoimport --db=gump --collection=users --drop Data/users.json
