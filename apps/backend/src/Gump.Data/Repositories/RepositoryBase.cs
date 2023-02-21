aef35e9eca40e8c49bbc73e09e55cbd0f0e18a72
Hegyi Áron Ferenc <hegyi.aron101@gmail.com>
2023-02-21 10:43:50 +0100

    ez jobb

--- apps/backend/src/Gump.Data/Repositories/RepositoryBase.cs
+++ apps/backend/src/Gump.Data/Repositories/RepositoryBase.cs
@@ -24,8 +24,21 @@ namespace Gump.Data.Repositories
 
 		protected int GetId()
 		{
-			return Collection.AsQueryable().Select(x => x.Id).DefaultIfEmpty(0).Max() + 1;
-		}
+			// this only increments the largest id by 1
+			// return Collection.AsQueryable().Select(x => x.Id).DefaultIfEmpty(0).Max() + 1;
 
+			// search for all the unused ids and return the first one to use that
+			// get all the ids
+			var ids = Collection.AsQueryable().Select(x => x.Id).ToList();
+
+			// sort them
+			ids.Sort();
+
+			// find the first unused id with linq
+			var unusedId = ids.Select((id, index) => new { id, index }).FirstOrDefault(x => x.id != x.index + 1);
+
+			// if there is no unused id, return the max id + 1
+			return unusedId == null ? ids.Max() + 1 : unusedId.id;
+		}
 	}
 }
