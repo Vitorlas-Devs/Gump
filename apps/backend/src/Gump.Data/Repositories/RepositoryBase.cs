2aeb00f544b8a77426c9361fa8987280d725263b
Hegyi Áron Ferenc <hegyi.aron101@gmail.com>
2023-02-22 18:05:17 +0100

    id ulong fix

--- apps/backend/src/Gump.Data/Repositories/RepositoryBase.cs
+++ apps/backend/src/Gump.Data/Repositories/RepositoryBase.cs
@@ -6,7 +6,7 @@ public interface IEntity
 {
 	// The Id property is required for all entities, because it is used to get the next available Id in the GetId() method
 	// so all models must implement this interface, sorry
-	int Id { get; set; }
+	ulong Id { get; set; }
 }
 public class RepositoryBase<T> where T : IEntity
 {
@@ -19,7 +19,7 @@ public class RepositoryBase<T> where T : IEntity
 		this.dbClient = new MongoClient(connectionString);
 	}
 
-	protected int GetId()
+	protected ulong GetId()
 	{
 		// Get all the Ids of the documents in the collection as a list
 		var ids = Collection.AsQueryable().Select(x => x.Id).ToList();
@@ -27,10 +27,10 @@ public class RepositoryBase<T> where T : IEntity
 
 		// Find the first unused Id, by finding the first Id that is not equal to its index + 1
 		// For example, if the Ids are [1, 2, 3, 5], then the first unused Id is 4
-		var unusedId = ids.Select((id, index) => new { id, index }).FirstOrDefault(x => x.id != x.index + 1);
+		var unusedId = ids.Select((id, index) => new { id, index }).FirstOrDefault(x => x.id != (ulong)x.index + 1);
 
 		// Return the first unused Id, or the maximum Id + 1 if there are no unused Ids
-		return unusedId == null ? ids.Max() + 1 : unusedId.id;
+		return unusedId != null ? unusedId.id : (ulong)ids.Count + 1;
 	}
 }
 
