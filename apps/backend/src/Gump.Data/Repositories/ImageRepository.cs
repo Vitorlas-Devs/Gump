8f71dc341e9e13d35765926704feed7ab3efa964
Menyhárt Attila <menyhart.atika@gmail.com>
2023-02-26 17:13:20 +0100

    Empty ImageRepository

---
+++ apps/backend/src/Gump.Data/Repositories/ImageRepository.cs
@@ -0,0 +1,10 @@
+using Gump.Data.Models;
+using MongoDB.Driver;
+
+namespace Gump.Data.Repositories;
+
+public class ImageRepository // : RepositoryBase<ImageModel>
+{
+	public ImageRepository(string connectionString)// : base(connectionString) 
+	{ }
+}
