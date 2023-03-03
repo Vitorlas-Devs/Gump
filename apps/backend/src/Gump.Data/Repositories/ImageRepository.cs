364f5829cbaed36c7876ac8bc1b50f0d56fd7da3
Menyhárt Attila <menyhart.atika@gmail.com>
2023-03-03 13:17:34 +0100

    ImageRepository kész

--- apps/backend/src/Gump.Data/Repositories/ImageRepository.cs
+++ apps/backend/src/Gump.Data/Repositories/ImageRepository.cs
@@ -3,8 +3,47 @@ using MongoDB.Driver;
 
 namespace Gump.Data.Repositories;
 
-public class ImageRepository // : RepositoryBase<ImageModel>
+public class ImageRepository : RepositoryBase<ImageModel>
 {
-	public ImageRepository(string connectionString)// : base(connectionString) 
-	{ }
+	private readonly UserRepository userRepository;
+
+	public ImageRepository(string connectionString) : base(connectionString)
+	{
+		this.userRepository = new(connectionString);
+	}
+
+	public ImageModel Create(ImageModel image)
+	{
+		image.Id = GetId();
+
+		ValidateFields(image, "Image");
+
+		if (image.OwnerId.HasValue)
+		{
+			userRepository.GetById(image.OwnerId.Value);
+		}
+
+		try
+		{
+			Collection.InsertOne(image);
+		}
+		catch (MongoException ex)
+		{
+			throw new AggregateException("Error while creating image", ex);
+		}
+
+		return image;
+	}
+
+	public void Delete(ulong id)
+	{
+		try
+		{
+			Collection.DeleteOne(x => x.Id == id);
+		}
+		catch (MongoException ex)
+		{
+			throw new AggregateException("Error while deleting image", ex);
+		}
+	}
 }
