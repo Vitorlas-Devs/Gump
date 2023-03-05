8b5b40790b830b6e4261a2a8a996fe5fa773eed3
Hegyi Áron Ferenc <hegyi.aron101@gmail.com>
2023-03-05 18:34:24 +0100

    windi

--- apps/frontend/Gump/vite.config.ts
+++ apps/frontend/Gump/vite.config.ts
@@ -2,13 +2,14 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import WindiCSS from 'vite-plugin-windicss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+	plugins: [vue(), WindiCSS()],
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url))
+		}
+	}
 })
