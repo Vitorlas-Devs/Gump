341830f8986370cd3ea0f090f8533e0022b7a529
Hegyi Áron Ferenc <hegyi.aron101@gmail.com>
2023-03-13 08:47:43 +0100

    tech

--- apps/backend/src/Gump.Data/Helpers/HashHelper.cs
+++ apps/backend/src/Gump.Data/Helpers/HashHelper.cs
@@ -1,28 +1,18 @@
 using System.Security.Cryptography;
 using System.Text;
+using BCrypt.Net;
 
 namespace Gump.Data.Helpers;
 
 public static class HashHelper
 {
-	public static string ComputeHash(string password, string salt, string pepper, int iterations)
+		public static string ComputeHash(string password, string salt, string pepper, int iterations)
 	{
-		if (iterations <= 0)
-		{
-			return password;
-		}
-		var passwordSaltPepper = $"{password}{salt}{pepper}";
-		var byteValue = Encoding.UTF8.GetBytes(passwordSaltPepper);
-		var byteHash = SHA256.HashData(byteValue);
-		var hash = Convert.ToBase64String(byteHash);
-		return ComputeHash(hash, salt, pepper, iterations - 1);
+		return BCrypt.Net.BCrypt.HashPassword($"{password}{salt}{pepper}", iterations);
 	}
 
 	public static string GenerateSalt()
 	{
-		using var rng = RandomNumberGenerator.Create();
-		var saltByte = new byte[16];
-		rng.GetBytes(saltByte);
-		return Convert.ToBase64String(saltByte);
+		return BCrypt.Net.BCrypt.GenerateSalt();
 	}
 }
