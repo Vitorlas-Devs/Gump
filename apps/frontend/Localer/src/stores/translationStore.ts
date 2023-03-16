7cef7840cdc7860bbcb26aef879ac7427232b82e
Hegyi Áron Ferenc <hegyi.aron101@gmail.com>
2023-03-16 13:43:58 +0100

    no harding (hard coding)

--- apps/frontend/Localer/src/stores/translationStore.ts
+++ apps/frontend/Localer/src/stores/translationStore.ts
@@ -1,10 +1,18 @@
 import { defineStore } from 'pinia'
 
+// en_US.json
+// {
+// 	"hoarderBadgeName": "Recipe Hoarder",
+// 	"hoarderBadgeDescription": "Save at least 100 recipes.",
+// 	"HomeButton": "Home",
+// 	"Welcome": "Welcome, {username}!"
+// }
+
 export const useTranslationStore = defineStore({
   id: 'translation',
   state: () => ({
-    locales: ['en_US', 'ko_KR', 'hu_HU'],
-    keys: ['hoarderBadgeName', 'hoarderBadgeDescription', 'HomeButton', 'Welcome'],
+    locales: [] as string[],
+    keys: [] as string[],
     translations: {} as Record<string, Record<string, string>>
   }),
   getters: {
@@ -20,13 +28,15 @@ export const useTranslationStore = defineStore({
       const translationModules = import.meta.glob('../../../../../locales/*.json')
       const translations = await Promise.all(
         Object.entries(translationModules).map(async ([path, loader]) => {
-          const locale = path.match(/\/(\w+)\.json$/)?.[1] ?? ''
+          const locale = path.match(/\/(\w+)\.json$/)?.[1] ?? 'en_US'
           const module = (await loader()) as { default: Record<string, string> }
 
           return [locale, module.default] as const
         })
       )
       this.translations = Object.fromEntries(translations)
+      this.locales = Object.keys(this.translations)
+      this.keys = Object.keys(this.translations[this.locales[0]] ?? {})
     },
     checkKey(key: string) {
       return this.translationsForKey(key)[this.locales[0]] !== null
