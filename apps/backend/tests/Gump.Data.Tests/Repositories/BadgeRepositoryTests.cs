bbf81194fc812e27c240ba55b00987e82a6f9256
Csepi Szilveszter <csepi.szilveszter@gmail.com>
2023-03-16 09:36:23 +0100

    BadgeRepo tests

---
+++ apps/backend/tests/Gump.Data.Tests/Repositories/BadgeRepositoryTests.cs
@@ -0,0 +1,159 @@
+namespace Gump.Data.Tests.Repositories;
+
+public class BadgeRepositoryTests : RepositoryTestsBase, IClassFixture<RepositoryTestsBase>
+{
+	private readonly RepositoryTestsBase fixture;
+
+	public BadgeRepositoryTests(RepositoryTestsBase fixture)
+	{
+		this.fixture = fixture;
+	}
+
+	[Fact]
+	public void GetById_Works()
+	{
+		// Arrange & Act
+		ImageModel image = Get<ImageModel>();
+		fixture.ImageRepository.Create(image);
+
+		BadgeModel badge = Get<BadgeModel>();
+		fixture.BadgeRepository.Create(badge);
+
+		BadgeModel badge2 = fixture.BadgeRepository.GetById(badge.Id);
+
+		// Assert
+		Assert.Equal(badge.Id, badge2.Id);
+		Assert.Equal(badge.Name, badge2.Name);
+	}
+
+	[Theory]
+	[InlineData("First")]
+	public void Create_CannotCreateDuplicate(string name)
+	{
+		// Arrange
+		ImageModel image = Get<ImageModel>();
+		fixture.ImageRepository.Create(image);
+
+		BadgeModel badge = Get<BadgeModel>();
+
+		// Act
+		badge.Name = name;
+		fixture.BadgeRepository.Create(badge);
+
+		// Assert
+		Assert.Equal(name, badge.Name);
+		Assert.Throws<ArgumentException>(() => fixture.BadgeRepository.Create(badge));
+	}
+
+	[Fact]
+	public void Create_CannotCreateWithNonExistentImage()
+	{
+		// Arrange & Act
+		BadgeModel badge = Get<BadgeModel>();
+
+		// Assert
+		Assert.Throws<ArgumentNullException>(() => fixture.BadgeRepository.Create(badge));
+	}
+
+	[Fact]
+	public void Create_CannotCreateWithEmptyName()
+	{
+		// Arrange
+		
+		BadgeModel badge = Get<BadgeModel>();
+
+		// Act
+		badge.Name = string.Empty;
+
+		// Assert
+		Assert.Throws<ArgumentNullException>(() => fixture.BadgeRepository.Create(badge));
+	}
+
+	[Fact]
+	public void Create_CannotCreateWithEmptyDescription()
+	{
+		// Arrange
+		BadgeModel badge = Get<BadgeModel>();
+
+		// Act
+		badge.Description = string.Empty;
+
+		// Assert
+		Assert.Throws<ArgumentNullException>(() => fixture.BadgeRepository.Create(badge));
+	}
+
+	[Theory]
+	[InlineData("First", "Second")]
+	public void Update_CannotUpdateToExistingName(string name, string name2)
+	{
+		//Arrange
+		ImageModel image = Get<ImageModel>();
+		fixture.ImageRepository.Create(image);
+
+		BadgeModel badge = Get<BadgeModel>();
+		badge.Name = name;
+		fixture.BadgeRepository.Create(badge);
+
+		BadgeModel badge2 = Get<BadgeModel>();
+		badge2.Name = name2;
+		fixture.BadgeRepository.Create(badge2);
+
+		// Act
+		badge.Name = name2;
+
+		// Assert
+		Assert.Equal(name2, badge.Name);
+		Assert.Equal(name2, badge2.Name);
+		Assert.Throws<ArgumentException>(() => fixture.BadgeRepository.Update(badge));
+	}
+
+	[Theory]
+	[InlineData("First")]
+	public void Delete_Works(string name)
+	{
+		// Arrange
+		ImageModel image = Get<ImageModel>();
+		fixture.ImageRepository.Create(image);
+
+		BadgeModel badge = Get<BadgeModel>();
+		badge.Name = name;
+		fixture.BadgeRepository.Create(badge);
+
+		CategoryModel category = fixture.CategoryRepository.Create(name);
+
+		UserModel user = Get<UserModel>();
+		fixture.UserRepository.Create(user);
+
+		RecipeModel recpie = Get<RecipeModel>();
+		fixture.RecipeRepository.Create(recpie);
+
+		// Act
+		fixture.BadgeRepository.Delete(badge.Id);
+
+		// Assert
+		Assert.Throws<ArgumentNullException>(() => fixture.BadgeRepository.GetById(badge.Id));
+
+		Assert.Throws<ArgumentNullException>(() => fixture.ImageRepository.GetById(image.Id));
+
+		Assert.DoesNotContain(badge.Id, fixture.UserRepository.GetById(user.Id).Badges);
+	}
+
+	[Theory]
+	[InlineData("First")]
+	public void Delete_CannotDeleteNonExistent(string name)
+	{
+		//Arrange
+		ImageModel image = Get<ImageModel>();
+		fixture.ImageRepository.Create(image);
+
+		BadgeModel badge = Get<BadgeModel>();
+		badge.Name = name;
+		fixture.BadgeRepository.Create(badge);
+
+		// Act
+		fixture.BadgeRepository.Delete(badge.Id);
+
+		// Assert
+		Assert.Throws<ArgumentNullException>(() => fixture.BadgeRepository.Delete(badge.Id));
+	}
+}
