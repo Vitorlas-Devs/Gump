fe961f0d6f9de7fa7e5b9ab09e46a08d6d5105db
Hegyi Áron Ferenc <hegyi.aron101@gmail.com>
2023-03-17 20:15:55 +0100

    cooler

--- apps/frontend/Localer/src/views/HomeView.vue
+++ apps/frontend/Localer/src/views/HomeView.vue
@@ -1,10 +1,8 @@
 <script setup lang="ts">
-import TheNavigation from '@/components/TheNavigation.vue'
 </script>
 
 <template>
   <main class="flex flex-row w-full h-screen">
-    <TheNavigation />
     <div class="flex flex-col w-full h-full p-4">
       <h1>Home</h1>
       <p class="text-gray-400">Welcome to the home page.</p>
---
+++ apps/frontend/Localer/src/views/TranslateHomeView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import TheNavigation from '@/components/TheNavigation.vue'
+</script>
+
+<template>
+  <main class="flex flex-row w-full h-screen">
+    <TheNavigation />
+    <div class="flex flex-col w-full h-full p-4">
+      <h1>Translate</h1>
+      <h3 class="text-gray-400">Choose your language.</h3>
+      <p class="text-gray-400">And select a key from the left sidebar.</p>
+    </div>
+  </main>
+</template>
+
+<style scoped></style>
