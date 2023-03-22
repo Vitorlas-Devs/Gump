06031b504c2200990339a8be2b4abe079f17231b
Hegyi Áron Ferenc <hegyi.aron101@gmail.com>
2023-03-22 18:09:43 +0100

    this is the best i could come up with so farr

--- apps/frontend/Localer/src/main.ts
+++ apps/frontend/Localer/src/main.ts
@@ -1,9 +1,26 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { useTranslationStore } from '@/stores/translationStore'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
+import { icon, library } from '@fortawesome/fontawesome-svg-core'
+// import { fas } from '@fortawesome/free-solid-svg-icons'
+// import { far } from '@fortawesome/free-regular-svg-icons'
+// import { fab } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+const x = 'faRotate'
+let definition
+
+// see: https://stackoverflow.com/questions/75281809/lazy-loading-fontawesome-icons-in-vue3-vite-not-working-in-dev
+if (import.meta.env.PROD) {
+  const iconModule = await import(`../node_modules/@fortawesome/free-solid-svg-icons/${x}.js`)
+  definition = iconModule.definition
+} else {
+  const iconModule = await import(`@fortawesome/free-solid-svg-icons`)
+  definition = iconModule[x as keyof typeof iconModule]
+}
+library.add(definition)
+// Property 'value' does not exist on type '(icon: IconLookup | IconName, params?: IconParams | undefined) => Icon'.ts(2339)
+// icon.value = 'fa-solid fa-rotate'
 
 import router from './router'
 import 'virtual:windi.css'
@@ -23,7 +40,7 @@ app.use(router)
 
 app.component(CustomScrollbar.name, CustomScrollbar)
 
-library.add(fas)
+// library.add(fas, far, fab)
 app.component('FontAwesomeIcon', FontAwesomeIcon)
 
 app.mount('#app')
