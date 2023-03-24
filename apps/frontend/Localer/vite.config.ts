9aec4dccb95c653937ee8a8c3c6d6e36837f4961
Hegyi Áron Ferenc <hegyi.aron101@gmail.com>
2023-03-24 11:37:40 +0100

    WTF

--- apps/frontend/Localer/vite.config.ts
+++ apps/frontend/Localer/vite.config.ts
@@ -1,41 +1,61 @@
 import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import WindiCSS from 'vite-plugin-windicss'
 import { VitePluginFonts } from 'vite-plugin-fonts'
 import svgLoader from 'vite-svg-loader'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
-interface ViteEnv {
-  [key: string]: string
-}
-
-const viteEnv: ViteEnv = {}
-
-Object.keys(process.env).forEach((key) => {
-  if (key.startsWith('VITE_')) {
-    viteEnv[`import.meta.env.${key}`] = process.env[key]!
-  }
-})
-
-export default defineConfig({
-  plugins: [
-    vue(),
-    WindiCSS(),
-    VitePluginFonts({
-      google: {
-        families: ['Ubuntu']
+export default ({ mode, command }: { mode: string; command: string }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  if (command === 'serve') {
+    return defineConfig({
+      define: {
+        'process.env.VITE_REPO': `"${env.VITE_REPO}"`,
+        'process.env.VITE_DEV': 'true'
+      },
+      plugins: [
+        vue(),
+        WindiCSS(),
+        VitePluginFonts({
+          google: {
+            families: ['Ubuntu']
+          }
+        }),
+        svgLoader(),
+        nodePolyfills({
+          protocolImports: true
+        })
+      ],
+      resolve: {
+        alias: {
+          '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
       }
-    }),
-    svgLoader(),
-    nodePolyfills({
-      protocolImports: true
     })
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+  }
+  return defineConfig({
+    define: {
+      'process.env.VITE_REPO': `'process.env.VITE_REPO'`,
+      'process.env.VITE_DEV': 'false'
+    },
+    plugins: [
+      vue(),
+      WindiCSS(),
+      VitePluginFonts({
+        google: {
+          families: ['Ubuntu']
+        }
+      }),
+      svgLoader(),
+      nodePolyfills({
+        protocolImports: true
+      })
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
-  },
-  define: viteEnv,
-})
+  })
+}
