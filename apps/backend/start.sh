941e989d36fad0a7a9c3d82c7bad55b44a192411
Menyhárt Attila <menyhart.atika@gmail.com>
2023-04-09 14:45:39 +0200

    Changed start.sh

--- apps/backend/start.sh
+++ apps/backend/start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-git pull
+wget https://raw.githubusercontent.com/14A-A-Lyedlik-Devs/Gump/main/apps/backend/docker-compose.yml
+wget https://raw.githubusercontent.com/14A-A-Lyedlik-Devs/Gump/main/apps/backend/start.sh
+chmod +x ./start.sh
 sudo docker compose down --volumes --remove-orphans
 sudo docker compose up -d --build --force-recreate
