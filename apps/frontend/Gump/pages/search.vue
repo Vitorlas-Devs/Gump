165cd138ece7d9335d053b2e3fdf31a7003ba5e6
Hegyi Áron Ferenc <hegyi.aron101@gmail.com>
2023-04-23 19:44:40 +0200

    done + tests

--- apps/frontend/Gump/pages/search.vue
+++ apps/frontend/Gump/pages/search.vue
@@ -1,12 +1,18 @@
 <script setup lang="ts">
+import { useUIStore } from '~/stores/ui'
 
+const ui = useUIStore()
 </script>
 
 <template>
   <ion-page bg-crimson-50>
-    <SearchHeader />
+    <SearchHeader z-2 />
     <div grow />
-    <TheNavbar />
+    <TheNavbar z-2 />
+    <div
+      v-if="ui.dropdownToggled"
+      absolute left-0 top-0 h-full w-full bg-brown-900 opacity-20
+    />
   </ion-page>
 </template>
 
---
+++ apps/frontend/Gump/tests/SearchHeader.nuxt.test.ts
@@ -0,0 +1,65 @@
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
+import { type VueWrapper, mount } from '@vue/test-utils'
+import { createI18n } from 'vue-i18n'
+import SearchHeader from '~/components/SearchHeader.vue'
+import { useUIStore } from '~/stores/ui'
+
+describe('SearchHeader', () => {
+  let wrapper: VueWrapper<InstanceType<typeof SearchHeader>>
+
+  beforeEach(() => {
+    const i18n = createI18n ({
+      messages: {
+        en: {
+          tip: 'Tip',
+        },
+        ch: {
+          tip: '提示',
+        },
+      },
+    })
+    wrapper = mount(SearchHeader, {
+      attachTo: document.body,
+      global: {
+        plugins: [i18n],
+      },
+    })
+  })
+
+  afterEach(() => {
+    vi.resetAllMocks()
+  })
+
+  it('should render the title when ui.searchToggled is false', async () => {
+    const ui = useUIStore()
+    ui.searchToggled = false
+    await wrapper.vm.$nextTick()
+    expect(wrapper.html()).toContain('SearchNav')
+  })
+
+  it('should render the input when ui.searchToggled is true', async () => {
+    const ui = useUIStore()
+    ui.searchToggled = true
+    await wrapper.vm.$nextTick()
+    expect(wrapper.find('input').exists()).toBe(true)
+  })
+
+  // this breaks vitest and it runs forever
+  // it('should focus on the input when ui.searchToggled is true', async () => {
+  //   const ui = useUIStore()
+  //   ui.searchToggled = true
+  //   await wrapper.vm.$nextTick()
+  //   const input = wrapper.find('input')
+  //   expect(input.element).toBe(document.activeElement)
+  // })
+
+  it('should update ui.searchValue with the input value', async () => {
+    const ui = useUIStore()
+    ui.searchToggled = true
+    await wrapper.vm.$nextTick()
+    const input = wrapper.find('input')
+    input.element.value = 'test'
+    await input.trigger('input')
+    expect(ui.searchValue).toBe('test')
+  })
+})
