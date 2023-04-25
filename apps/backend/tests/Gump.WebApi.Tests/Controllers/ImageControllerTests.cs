88ccd8c6643448c23abea897c9aa0d911fc621ba
Menyhárt Attila <menyhart.atika@gmail.com>
2023-04-25 16:04:03 +0200

    ImageController Tests done

--- apps/backend/tests/Gump.WebApi.Tests/Controllers/ImageControllerTests.cs
+++ apps/backend/tests/Gump.WebApi.Tests/Controllers/ImageControllerTests.cs
@@ -4,16 +4,16 @@ using Microsoft.AspNetCore.Mvc;
 namespace Gump.WebApi.Tests.Controllers;
 
 [Collection("ControllerTests")]
-public class PartnerControllerTests : ControllerTestsBase
+public class ImageControllerTests : ControllerTestsBase
 {
 	[Fact]
 	public void GetById_Works()
 	{
 		// Arrange
-		var controller = new PartnerController(PartnerRepository, UserRepository);
+		var controller = new ImageController(ImageRepository, UserRepository);
 
 		// Act
-		var result = controller.GetPartner(1);
+		var result = controller.GetImage(1);
 
 		// Assert
 		Assert.NotNull(result);
