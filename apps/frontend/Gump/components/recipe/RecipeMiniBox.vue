b473c105e5ae8c144661d476b72226eca00a8834
Csepi Szilveszter <csepi.szilveszter@gmail.com>
2023-04-30 19:32:16 +0200

    RecipeMiniBox

---
+++ apps/frontend/Gump/components/recipe/RecipeMiniBox.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+defineProps<{
+  title: string
+  authorId: number
+  imgId: number
+}>()
+
+const image = useImageStore()
+const user = useUserStore()
+const ui = useUIStore()
+const recipe = useRecipeStore()
+
+function addRecipe(recipeId: number) {
+  recipe.addRecipe(recipeId)
+}
+</script>
+
+<template>
+  <div flex="~" shadow-orangebox m-4 max-h-35 w-auto rounded-2xl bg-orange-50>
+    <img :src="image.getImage(imgId)" w="1/3" rounded-s-2xl object-cover>
+    <div grow self-center text-shadow>
+      <p m-3 text-xl>
+        {{ title }}
+      </p>
+      <p m-3 text-lg>
+        {{ user.username }}
+      </p>
+    </div>
+    <img v-if="ui.activeNav === 'Create'" src="~assets/PlusSign.svg" m-3 @click="addRecipe(1)">
+  </div>
+</template>
+
+<style scoped></style>
