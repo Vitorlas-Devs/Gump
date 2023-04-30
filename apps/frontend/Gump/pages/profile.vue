9111ce93138f0b04f1279dc2cf691ac15dd608fb
Csepi Szilveszter <csepi.szilveszter@gmail.com>
2023-04-30 19:56:40 +0200

    Hotfix

--- apps/frontend/Gump/pages/profile.vue
+++ apps/frontend/Gump/pages/profile.vue
@@ -7,7 +7,7 @@ const imgId = 4
 
 <template>
   <ion-page bg-crimson-50>
-    <RecipeMiniBox :title="title" :author-id="authorId" :img-id="imgId" />
+    <RecipeBoxMini :title="title" :author-id="authorId" :img-id="imgId" />
     <RecipeBox :title="title" :author-id="authorId" :img-id="imgId" />
     <TheNavbar />
   </ion-page>
