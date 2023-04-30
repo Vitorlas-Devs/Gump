ae9f142d93af9143deb55102fa14000286c25fee
Csepi Szilveszter <csepi.szilveszter@gmail.com>
2023-04-30 19:32:54 +0200

    Place mini recipe to profile

--- apps/frontend/Gump/pages/profile.vue
+++ apps/frontend/Gump/pages/profile.vue
@@ -1,22 +1,14 @@
 <script setup lang="ts">
 const user = useUserStore()
+const title = 'Nagyon Fincsi pogácsa'
+const authorId = 1
+const imgId = 4
 </script>
 
 <template>
   <ion-page bg-crimson-50>
-    <TheHeader show-moderator :title="$t('ProfileNav')" />
-    <div grow>
-      <h1 text-crimson-500 text-shadow-crimson>
-        {{ $t('ProfileNav') }}
-      </h1>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil voluptatum similique explicabo suscipit eum alias doloremque quidem iure corrupti pariatur. Explicabo neque assumenda ipsa, error accusamus vel recusandae aliquid doloremque.
-      <br><br>
-      Select Language:
-      <LanguageSwitcher />
-      <br><br>
-      Change username:
-      <input v-model="user.username">
-    </div>
+    <RecipeMiniBox :title="title" :author-id="authorId" :img-id="imgId" />
+    <RecipeBox :title="title" :author-id="authorId" :img-id="imgId" />
     <TheNavbar />
   </ion-page>
 </template>
