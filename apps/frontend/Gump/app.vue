230b698c4b3248e2c608ee4337f31b070fe36a20
Csepi Szilveszter <csepi.szilveszter@gmail.com>
2023-05-03 00:19:49 +0200

    GetUserData

--- apps/frontend/Gump/app.vue
+++ apps/frontend/Gump/app.vue
@@ -1,3 +1,13 @@
+<script setup lang="ts">
+const user = useUserStore()
+const recipe = useRecipeStore()
+
+watch(recipe.recipes,
+  async () => await user.getUserData(),
+  { deep: true, immediate: true },
+)
+</script>
+
 <template>
   <ion-app>
     <ion-router-outlet font-sans text-brown-500 :animated="false" />
