14348cc53170c64e1821ccdc5c82b83ca1d74506
Csepi Szilveszter <csepi.szilveszter@gmail.com>
2023-05-03 00:17:28 +0200

    Like és save

--- apps/frontend/Gump/components/recipe/RecipeBox.vue
+++ apps/frontend/Gump/components/recipe/RecipeBox.vue
@@ -1,39 +1,51 @@
 <script setup lang="ts">
-defineProps<{
+const props = defineProps<{
   title: string
   authorId: number
   imgId: number
+  views: number
+  likes: number
+  saves: number
+  id: number
 }>()
 
 const image = useImageStore()
+const localePath = useLocalePath()
+const ui = useUIStore()
 const user = useUserStore()
+const recipe = useRecipeStore()
 
 const isLiked = ref(false)
 const isSaved = ref(false)
+const authorName = ref('')
+
+async function viewRecipe(recipeId: number) {
+  ui.activeNav = 'Recipes'
+  await navigateTo(localePath(`/recipe/${recipeId}`))
+}
+
+onMounted(async () => {
+  authorName.value = await user.getAuthorNameById(props.authorId)
+  isLiked.value = await recipe.getRecipeById(props.id).then(r => r.isLiked)
+  isSaved.value = user.recipes.includes(props.id)
+})
 </script>
 
 <template>
   <div flex="~ col" shadow-orangeBox m-4 h-262px rounded-2xl bg-orange-50>
-    <img
-      :src="image.getImage(imgId)"
-      h="367/593" w-full self-center rounded-t-2xl object-cover
-    >
+    <img :src="image.getImage(imgId)" h="367/593" w-full cursor-pointer self-center rounded-t-2xl object-cover @click="viewRecipe(id)">
     <div flex="~ col" shadow-orangeImage px-1 text-center text-shadow>
-      <p my-1 truncate text-xl>
-        {{ title }}
-      </p>
-      <p my-1 truncate text-lg>
-        {{ user.username }}
-      </p>
-      <RecipeFooter
-        :view-count="32500000" :like-count="1234" :save-count="123456789"
-        :is-liked="isLiked" :is-saved="isSaved"
-        @like="isLiked = !isLiked" @save="isSaved = !isSaved"
-      />
+      <div cursor-pointer @click="viewRecipe(id)">
+        <p my-1 truncate text-xl>
+          {{ title }}
+        </p>
+        <p my-1 truncate text-lg>
+          {{ authorName }}
+        </p>
+      </div>
+      <RecipeFooter :id="id" :view-count="views" :like-count="likes" :save-count="saves" :is-liked="isLiked" :is-saved="isSaved" @like="isLiked = !isLiked" @save="isSaved = !isSaved" />
     </div>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
