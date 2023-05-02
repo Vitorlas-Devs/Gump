44f80e9b0b84efa12e1ebc40c0212949349096d3
Csepi Szilveszter <csepi.szilveszter@gmail.com>
2023-05-03 00:19:18 +0200

    Watch activeSort, receptek feltöltése

--- apps/frontend/Gump/pages/home.vue
+++ apps/frontend/Gump/pages/home.vue
@@ -1,31 +1,31 @@
 <script setup lang="ts">
-const { currentRecipe } = toRefs(useRecipeStore())
+const recipe = useRecipeStore()
+const ui = useUIStore()
+
+watch(
+  () => ui.activeSort,
+  async () => await recipe.getRecipes(ui.activeSort),
+  { immediate: true },
+)
+
+ui.activeNav = 'Home'
 </script>
 
 <template>
   <ion-page bg-crimson-50>
     <TheHeader show-icons :title="$t('HomeNav')" />
-    <div v-if="currentRecipe" grow overflow-y-auto pb-30>
-      <RecipeBox title="Nagyon nagyon nagyon naon fincsi pogi" :author-id="currentRecipe.author" :img-id="currentRecipe.image" />
-      <RecipeBox title="Nagyon nagyon nagyon naon fincsi pogi" :author-id="currentRecipe.author" :img-id="currentRecipe.image" />
+    <div v-if="recipe.recipes" grow overflow-y-auto pb-30>
+      <RecipeBox v-for="r of recipe.recipes" :id="r.id" :key="r.id" :title="r.title" :author-id="r.author" :img-id="r.image" :views="r.viewCount" :likes="r.likeCount" :saves="r.saveCount" />
       <br><br>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil voluptatum similique explicabo suscipit eum alias doloremque quidem iure corrupti pariatur. Explicabo neque assumenda ipsa, error accusamus vel recusandae aliquid doloremque.
       <br>
       <div mt-5 class="crimsonBtn">
         <div class="i-fa6-solid-box-tissue" />
       </div>
-      <TextInput v-model:text="currentRecipe.title" />
-      <SearchSelect
-        v-model:model="currentRecipe.tags"
-        :options="currentRecipe.tags"
-        mode="tags"
-      />
       <MainButton fixed color="orange" icon-type="create" title="Create recipe" />
     </div>
     <TheNavbar />
   </ion-page>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
