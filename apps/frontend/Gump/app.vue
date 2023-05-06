79fa1d624cf14ef6886a0ae0b468ab828d3881f5
Csepi Szilveszter <csepi.szilveszter@gmail.com>
2023-05-06 15:17:20 +0200

    404 fix

--- apps/frontend/Gump/app.vue
+++ apps/frontend/Gump/app.vue
@@ -1,13 +1,3 @@
-<script setup lang="ts">
-const user = useUserStore()
-const recipe = useRecipeStore()
-
-watch(recipe.recipes,
-  async () => await user.getUserData(),
-  { deep: true, immediate: true },
-)
-</script>
-
 <template>
   <ion-app>
     <ion-router-outlet font-sans text-brown-500 :animated="false" />
