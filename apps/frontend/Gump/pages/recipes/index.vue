498ec8c6067df597b98ea6998eab7c4a7141dfff
Csepi Szilveszter <csepi.szilveszter@gmail.com>
2023-05-15 22:14:29 +0200

    Hotfix

--- apps/frontend/Gump/pages/recipes/index.vue
+++ apps/frontend/Gump/pages/recipes/index.vue
@@ -19,13 +19,13 @@ watch(
     <div grow overflow-y-auto>
       <div flex="~ row" my-10 items-center justify-around>
         <div :class="recipesSort === 'owned' ? 'bg-orange-500 text-white-500 drop-shadow-sm b-rd-25 drop-shadow-color-orange-500' : 'text-orange-500 text-shadow-orange'" cursor-pointer px-4 py-1 text-6 @click="recipesSort = 'owned'">
-          $t('RecipesSortOwned')
+          {{ $t('RecipesSortOwned') }}
         </div>
         <div :class="recipesSort === 'liked' ? 'bg-crimson-500 text-white-500 drop-shadow-sm b-rd-25 drop-shadow-color-crimson-500' : 'text-crimson-500 text-shadow-crimson'" cursor-pointer px-4 py-1 text-6 @click="recipesSort = 'liked'">
-          $t('RecipesSortLiked')
+          {{ $t('RecipesSortLiked') }}
         </div>
         <div :class="recipesSort === 'saved' ? 'bg-blue-500 text-white-500 drop-shadow-sm b-rd-25 drop-shadow-color-blue-500' : 'text-blue-500 text-shadow-blue'" cursor-pointer px-4 py-1 text-6 @click="recipesSort = 'saved'">
-          $t('RecipesSortSaved')
+          {{ $t('RecipesSortSaved') }}
         </div>
       </div>
       <RecipeBoxMini v-for="r in userRecipes" :key="r.id" :recipe="r" my-3 />
