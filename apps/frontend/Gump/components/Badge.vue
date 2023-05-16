ae2cfd479ef3925828320adb92358d5c764ccb7b
Csepi Szilveszter <csepi.szilveszter@gmail.com>
2023-05-16 23:01:59 +0200

    Badge

---
+++ apps/frontend/Gump/components/Badge.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+defineProps<{
+  badge: Badge
+}>()
+
+const image = useImageStore()
+</script>
+
+<template>
+  <div flex="~ col">
+    <div flex="~ row" items-center>
+      <img :src="image.getImageUrl(badge.image)" h-20 w-20>
+      <div ml-5 text-xl font-bold>
+        {{ $t(badge.name) }}
+      </div>
+    </div>
+    <p>
+      {{ $t(badge.description) }}
+    </p>
+  </div>
+</template>
