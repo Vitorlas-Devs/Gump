bfdee46ae76df0883c754f2d3237eb59e31e0c02
Csepi Szilveszter <csepi.szilveszter@gmail.com>
2023-05-16 23:02:22 +0200

    LanguageSwitcher

--- apps/frontend/Gump/components/LanguageSwitcher.vue
+++ apps/frontend/Gump/components/LanguageSwitcher.vue
@@ -1,28 +1,35 @@
 <script setup lang="ts">
 import type { LocaleObject } from '@nuxtjs/i18n/dist/runtime/composables'
-import { tabData } from '~/stores/ui'
 
 const { locales } = useI18n()
-const switchLocalePath = useSwitchLocalePath()
-const ui = useUIStore()
+const localePath = useLocalePath()
+const user = useUserStore()
 
 const localeObjects = locales.value as LocaleObject[]
 
-const route = useRoute()
+const localeNames = computed(() => {
+  return localeObjects.map(locale => locale.name) as string[]
+})
 
-const tabToRoute = tabData.find(
-  element => element.path === route.path.replace(/^\/\w+\//, '/'),
-)?.tab as Tab
-
-function setActive() {
-  ui.setActiveNav(tabToRoute)
-}
+const langs = computed({
+  get: () => {
+    const language = localeObjects.find(locale => locale.code === user.current.language)
+    return language?.name ?? ''
+  },
+  set: async (value: string) => {
+    const language = localeObjects.find(lang => lang.name === value)
+    if (language) {
+      user.current.language = language.code
+      await navigateTo(localePath('/profile', language.code))
+    } else {
+      user.current.language = ''
+    }
+  },
+})
 </script>
 
 <template>
   <div my-2 flex="~ row" gap-2>
-    <NuxtLink v-for="locale in localeObjects" :key="locale.code" :to="switchLocalePath(locale.code)" class="orangeLink" @click="setActive">
-      {{ locale.name }}
-    </NuxtLink>
+    <SearchSelect v-model:model="langs" w-60 :options="localeNames" mode="single" />
   </div>
 </template>
